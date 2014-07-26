0f011101e1dcceccb0fadd52f830f71c400e7242
ken.adams <ken.adams@aviacode.com>
2014-07-26 14:37:24 -0600

    routing fix

--- CpsCouponsSolution/CpsCouponsSolution/js/app.js
+++ CpsCouponsSolution/CpsCouponsSolution/js/app.js
@@ -1,18 +1,17 @@
 ﻿var app = angular.module('app', ['ngRoute', 'ui.bootstrap'])
 
-.config(['$routeProvider',
-  function($routeProvider, $locationProvider) {
+.config(function($routeProvider, $locationProvider) {
   	$routeProvider.
 	  when('/admin', {
-	  	templateUrl: 'views/admin.html',
+	  	templateUrl: '/views/admin.html',
 	  	controller: 'adminMenuCtrl'
 	  }).
 	  when('/adminprogram/:programId', {
-	  	templateUrl: 'views/program.html',
+	  	templateUrl: '/views/program.html',
 	  	controller: 'programCtrl'
 	  }).
 	  otherwise({
 	  	redirectTo: '/'
 	  });
 	  $locationProvider.html5Mode(true);
-  }]);
+  });
