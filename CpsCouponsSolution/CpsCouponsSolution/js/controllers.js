cb8917d05998f4fc77f65a35b12ac3eb186eab68
ken.adams <ken.adams@aviacode.com>
2014-07-26 19:29:22 -0600

    working on create program screen

--- CpsCouponsSolution/CpsCouponsSolution/js/controllers.js
+++ CpsCouponsSolution/CpsCouponsSolution/js/controllers.js
@@ -1,31 +1,29 @@
-﻿app.controller('adminMenuCtrl', function ($scope, programsApiService) {
-	$scope.programs = [{ name: 'program1', id: 1 }, { name: 'program2', id: 2 }];
-
-	$scope.getMallList = function () {
-		programsApiService.getMallList()
-			.then(function(data) {
-				$scope.malls = data;
-			})
-			.catch(function() {
-				addAlert('Unable to get Center data');
-			});
-	};
-
-	$scope.getMallList();
-		console.log($scope);
-
-	$scope.addAlert = function (message) {
-		$scope.alerts.push({ msg: message });
-	};
-
-	$scope.closeAlert = function (index) {
-		$scope.alerts.splice(index, 1);
-	};
-}
-
+﻿app.controller('adminMenuCtrl', function($scope) {
+		$scope.programs = [{ name: 'program1', id: 1 }, { name: 'program2', id: 2 }];
+	}
 );
 
-app.controller('programCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+app.controller('programCtrl', function ($scope, $routeParams, programsApiService) {
 		$scope.programId = $routeParams.programId;
+
+		$scope.getMallList = function() {
+			programsApiService.getMallList()
+				.then(function(data) {
+					$scope.malls = data;
+				})
+				.catch(function() {
+					addAlert('Unable to get Center data');
+				});
+		};
+
+		$scope.getMallList();
+
+		$scope.addAlert = function(message) {
+			$scope.alerts.push({ msg: message });
+		};
+
+		$scope.closeAlert = function(index) {
+			$scope.alerts.splice(index, 1);
+		};
 	}
-]);
+);
