c3b1e93bc861ca5b778d4f3128ffb1e44a8025ec
ken.adams <ken.adams@aviacode.com>
2014-07-26 18:18:27 -0600

    updating services

--- CpsCouponsSolution/CpsCouponsSolution/js/controllers.js
+++ CpsCouponsSolution/CpsCouponsSolution/js/controllers.js
@@ -1,7 +1,29 @@
-﻿app.controller('adminMenuCtrl', ['$scope', '$http', function($scope, $http) {
-		$scope.programs = [{name: 'program1', id: 1 }, {name: 'program2', id: 2}];
-	}
-]);
+﻿app.controller('adminMenuCtrl', function ($scope, programsApiService) {
+	$scope.programs = [{ name: 'program1', id: 1 }, { name: 'program2', id: 2 }];
+
+	$scope.getMallList = function () {
+		programsApiService.getMallList()
+			.then(function(data) {
+				$scope.malls = data;
+			})
+			.catch(function() {
+				addAlert('Unable to get Center data');
+			});
+	};
+
+	$scope.getMallList();
+		console.log($scope);
+
+	$scope.addAlert = function (message) {
+		$scope.alerts.push({ msg: message });
+	};
+
+	$scope.closeAlert = function (index) {
+		$scope.alerts.splice(index, 1);
+	};
+}
+
+);
 
 app.controller('programCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
 		$scope.programId = $routeParams.programId;
