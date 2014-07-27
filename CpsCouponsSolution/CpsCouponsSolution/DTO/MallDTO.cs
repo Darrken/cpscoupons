109740eaff6329330eb2f24f1ee32a4320eb544d
Rich.Watts <Rich.Watts@AviaCode.com>
2014-07-26 18:16:39 -0600

    Added ProgramService and MallDTO. Added StateId and MallId to return of programsController.GetMallsList()

---
+++ CpsCouponsSolution/CpsCouponsSolution/DTO/MallDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CpsCouponsSolution.DTO
+{
+	public class MallDTO
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int StateId { get; set; }
+		public string StateName { get; set; }
+	}
+}
---
+++ CpsCouponsSolution/CpsCouponsSolution/Services/ProgramsService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CpsCouponsSolution.DTO;
+using CpsCouponsSolution.Models;
+
+namespace CpsCouponsSolution.Services
+{
+	public class ProgramsService
+	{
+		public List<MallDTO> GetMallNames(bool isAll)
+		{
+			List<MallDTO> mallList;
+			using (var dbContext = new ToolkitEntities())
+			{
+				var malls = dbContext.Malls.Select(m => new MallDTO {
+																							Id = m.ID, 
+																							Name = m.Name, 
+																							StateId = m.State.ID, 
+																							StateName = m.State.Abbreviation
+																						});
+
+				if (!isAll)
+					malls = malls.Where(m => !m.Name.Contains("?")
+													 && !m.Name.ToLower().StartsWith("test")
+													 && !m.Name.ToLower().StartsWith("zz"));
+
+				mallList = malls.OrderBy(m => m.Name).ToList();
+			}
+
+			return mallList;
+		}
+
+		public int CreateProgram(ProgramDTO programData)
+		{
+			//using (var dbContext = new ToolkitEntities())
+			//{
+			//	var newProgram = new Program()
+			//						  {
+			//							  CouponWordCount = programData.CouponWordCount,
+					                 
+			//						  }
+			//}
+
+			throw new NotImplementedException();
+		}
+	}
+}
