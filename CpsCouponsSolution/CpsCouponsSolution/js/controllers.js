48d64eef6214867ae3d77d78347115faf051ab48
ken.adams <ken.adams@aviacode.com>
2014-07-26 21:49:01 -0600

    moved to angular route cdn, continuing work on create program

--- CpsCouponsSolution/CpsCouponsSolution/js/controllers.js
+++ CpsCouponsSolution/CpsCouponsSolution/js/controllers.js
@@ -1,29 +1,50 @@
-﻿app.controller('adminMenuCtrl', function($scope) {
-		$scope.programs = [{ name: 'program1', id: 1 }, { name: 'program2', id: 2 }];
-	}
-);
+﻿app.controller('adminMenuCtrl', function ($scope) {
+	$scope.programs = [{ name: 'program1', id: 1 }, { name: 'program2', id: 2 }];
+});
 
 app.controller('programCtrl', function ($scope, $routeParams, programsApiService) {
-		$scope.programId = $routeParams.programId;
+	$scope.program = {};
+	$scope.alerts = [];
+	$scope.program.ProgramId = $routeParams.programId;
+	$scope.program.Centers = [];
 
-		$scope.getMallList = function() {
-			programsApiService.getMallList()
-				.then(function(data) {
-					$scope.malls = data;
-				})
-				.catch(function() {
-					addAlert('Unable to get Center data');
-				});
-		};
+	$scope.getMallList = function () {
+		programsApiService.getMallList()
+			.then(function (data) {
+				$scope.malls = data;
+			})
+			.catch(function () {
+				addAlert('Unable to get Center data');
+			});
+	};
 
-		$scope.getMallList();
+	$scope.getMallList();
 
-		$scope.addAlert = function(message) {
-			$scope.alerts.push({ msg: message });
-		};
+	function addAlert(message) {
+		$scope.alerts.push({ type: 'danger', msg: message });
+	};
 
-		$scope.closeAlert = function(index) {
-			$scope.alerts.splice(index, 1);
-		};
-	}
-);
+	$scope.closeAlert = function (index) {
+		$scope.alerts.splice(index, 1);
+	};
+
+	$scope.toggleCenter = function (id) {
+		var idx = $scope.program.Centers.indexOf(id);
+
+		// is currently selected
+		if (idx > -1) {
+			$scope.program.Centers.splice(idx, 1);
+		}
+
+			// is newly selected
+		else {
+			$scope.program.Centers.push(id);
+		}
+	};
+
+	$scope.saveProgram = function () {
+		var toSubmit = {};
+
+
+	};
+});
--- CpsCouponsSolution/CpsCouponsSolution/js/services.js
+++ CpsCouponsSolution/CpsCouponsSolution/js/services.js
@@ -23,5 +23,31 @@
 			return deferred.promise;
 		},
 
+		createProgram: function (model) {
+			var deferred = $q.defer();
+			var apiUrl = this.baseQueriesApiUrl + 'createProgram';
+			
+			$http({
+				url: apiUrl,
+				method: 'POST',
+				dataType: 'json',
+				data: model
+			})
+				.success(function (data, status) {
+					data = data || {};
+					data.status = status;
+
+					deferred.resolve(data);
+				})
+				.error(function (data, status) {
+					data = data || {};
+					data.status = status;
+
+					deferred.reject(data);
+				});
+
+			return deferred.promise;
+		}
+
 	};
 });
